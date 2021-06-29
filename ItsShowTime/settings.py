bad10c46b92e5605606c73247e6c6ae4e6a13010
sudeeep885 <sudeep885@gmail.com>
2021-06-29 11:44:31 +0530

    added localhost in allowed hosts

--- ItsShowTime/settings.py
+++ ItsShowTime/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['itsshowtimedjango.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'itsshowtimedjango.pythonanywhere.com'
+    ]
 
 
 # Application definition
